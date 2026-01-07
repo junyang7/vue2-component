3b6310b29f4221219fc4504cb8cac3d077c931cc
junyang7 <junyang7@staff.weibo.com>
2026-01-07 13:08:16 +0800

    0.0.1+20260107130816

--- src/component/Loading/Loading.vue
+++ src/component/Loading/Loading.vue
@@ -49,7 +49,7 @@ export default {
     bottom: 0;
     width: 100%;
     height: 100%;
-    /*background-color: rgba(0,0,0,0.8);*/
+    background-color: rgba(0,0,0,0.8);
     display: flex;
     align-items: center;
     justify-content: center;
